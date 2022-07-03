d9b065931f5d9747000b36317c5938126c785a86
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-03 01:03:51 +0100

    Update Blog.jsx

--- client/src/Components/Blog.jsx
+++ client/src/Components/Blog.jsx
@@ -1,14 +1,70 @@
 import React from 'react'
+import img from '../Assests/blog.jpg';
 
 const Blog = () => {
   return (
     <div>
                 <div 
                 className='text-center'>
-                    <h3 
-                        className='text-3xl text-brightRed font-bold font-serif'>Our Blog  </h3>
+                    <h3  className='text-3xl text-brightRed font-bold font-serif'>Our Blog  </h3>
+                                <p>Just In </p>    
+              </div>
 
-            </div>
+
+
+                <div 
+                    className='mt-10'>
+                        
+                                        <ul 
+                                            className='mt-5 flex flex-col  md:flex-row justify-center h-1/2 p-5
+                                            mx-20 md:space-x-6'>
+                                                       
+                                                        <li 
+                                                            className='cursor-pointer  mt-3 md:w-1/3 border-2'>
+                                                                            <img src={img} alt=""
+                                                                            className='' />
+
+                                                                    <div
+                                                                     className='p-7'>
+                                                                    <p> 25 </p>
+                                                                    <p> May  </p>
+                                                                    <p>  Top 10 colors for the best lipstick </p>
+
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+                                                                    </div>
+                                                        </li>
+
+                                                        <li 
+                                                            className='cursor-pointer mt-3 md:w-1/3 border-2'>
+                                                                            <img src={img} alt=""
+                                                                            className='' />
+
+                                                                    <div
+                                                                     className='p-7'>
+                                                                    <p> 25 </p>
+                                                                    <p> May  </p>
+                                                                    <p>  Top 10 colors for the best lipstick </p>
+
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+                                                                    </div>
+                                                        </li>
+
+                                                
+                                                      
+
+                                          </ul>
+
+                            <div>
+                                    <button>
+                                             PREV
+                                    </button>
+
+                                    <button>
+                                          NEXT
+                                    </button>
+                            </div>
+
+                  </div>
 
     </div>
   )
