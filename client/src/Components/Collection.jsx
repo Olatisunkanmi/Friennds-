3c02026d559be950e82d190f2d0982ec04d81ebb
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-03 23:31:10 +0100

    Create Collection.jsx

---
+++ client/src/Components/Collection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+const Collection = () => {
+  return (
+    <div>
+    
+            <div
+                className=' collection--sidebar'> 
+                        <h3>
+                                    Collection 
+                        </h3>
+            </div>
+    </div>
+  )
+}
+
+export default Collection
