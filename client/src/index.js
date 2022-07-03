4d52e510de0d083bb57a9c84969a52fda599c55b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-03 23:30:50 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ContextProvider } from './Contexts/ContextProvider';
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ContextProvider>
+                <App />
+  </ContextProvider>
 );
 
