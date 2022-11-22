709be19b638162f7570cef7b1ec6e9a1e3826679
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-11-22 07:20:50 +0100

    Update PostsRoute.js

--- routes/PostsRoute.js
+++ routes/PostsRoute.js
@@ -2,10 +2,4 @@ const router = require('express').Router();
 const PostController = require('../Controllers/PostController');
 const { auth } = require('../miiddleware/auth');
 
-router
-	.route('/')
-	.get(PostController.getAllPosts)
-	.post(auth, PostController.createPosts);
-
-router.route('/:id').delete(PostController.deletePost);
 module.exports = router;
